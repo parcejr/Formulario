d5b75f9f713af9ca11c1a32b0e528229ac35804c
Jorge Tinoco <iorifonseca777@gmail.com>
2019-01-23 17:01:52 -0500

    actualizado la validación de la vista formulario

--- application/views/Formulario/formulario.php
+++ application/views/Formulario/formulario.php
@@ -6,39 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
-    <script src="assets/js/validacion.js"></script>
-    <style>
-        input[type=number]::-webkit-inner-spin-button,
-        input[type=number]::-webkit-outer-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-        }
-    </style>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <title>Consulta</title>
 </head>
 <body>
     
-    <div class="row container">
-        <div class="col-md-3">
-        </div>
+    <div class="row container" id="app">
+        <div class="col-md-3"></div>
         <div class="col-md-6 bordado">
             <h1 align="center">TRANSMISIÓN</h1>
-            <form action="index.php/Ejecutar/guardar" method = "POST" onsubmit="return validar()" id="form1">
+            <form  id="form1" @submit="revisar" action="index.php/Ejecutar/guardar" method="POST">
                 <label for="NOMBRE">NOMBRE:</label><br>
-                <input type="text" class="form form-control" id ="NOMBRE" name="nombre" required><br>
+                <input type="text" class="form form-control" id ="NOMBRE" name="nombre" v-model="nombre" ><br>
                 <label for="EDAD">EDAD:</label><br>
-                <input type="number" class="form form-control" id ="EDAD" name="edad" required> <br>
+                <input type="number" class="form form-control" id ="EDAD" name="edad" v-model="edad" > <br>
                 <label for="EMAIL">EMAIL</label>
-                <input type="text" class="form form-control" id= "EMAIL" name="email" required><br>
+                <input type="text"  class="form form-control" id= "EMAIL" name="email" v-model="email" ><br>
                 <label for="TELEFONO">TELEFONO</label>
-                <input type="number" class="form form-control" id = "TELEFONO" name="telefono" required><br>
+                <input type="number" class="form form-control" id = "TELEFONO" name="telefono" v-model.number="telefono"><br>
                 <label for="DIRECCION"> DIRECCION</label>
-                <input type="text" class="form form-control" id="DIRECCION" name="direccion" required><br>
+                <input type="text" class="form form-control" id="DIRECCION" name="direccion" v-model="direccion" ><br>
                 <label for="CELULAR"> CELULAR</label>
-                <input type="number" class="form form-control" id="CELULAR" name="celular" required><br>
+                <input type="number" class="form form-control" id="CELULAR" name="celular" v-model.number="celular" ><br>
                 <label for="select1"> AREA DE TRABAJO</label><br>
-                <select required name="area_trabajo" id="select1" class="custom-select mb-5">  
-                    <option select> SELECCIONE UNO</option> 
+                <select required name="area_trabajo" id="select1"  v-model="selected" class="custom-select mb-5">
                     <option value="EquiposElectromecanicos"> Equipos Electromecanicos</option>
                     <option value="EquiposElectromecanicosGestion">Equipos Electromecanicos Gestion</option>
                     <option value="GestionImplementación">Gestion Implementación</option>
@@ -47,13 +38,16 @@
                     <option value="RedTransporteCalidadDatosRF">Red De Transporte RF - Calidad Red De Datos </option>
                     <option value="RF-CalidadDatos-RF-PlaneacionDiseño"> RF - Calidad De Datos RF Planeación De Diseño</option>
                 </select> 
+                <div class="alert alert-warning" v-for="error of errores">{{error}}</div>
                 <input type="submit" value="Enviar" class="btn btn-primary float-right"><br>
             </form>
         </div>
         <div class="col-md-3"></div>
     </div>
+    <script src="assets/js/validacion.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    
 </body>
 </html>
--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -1,16 +1,41 @@
-(function() {
-    'use strict';
-  
-    window.addEventListener('load', function() {
-      var form = document.getElementById('form1');
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    }, false);
-  })();
+const vm = new Vue({
+  el:'#form1',
+  data:{
+    errores: [], 
+    nombre:null,
+    edad: null,
+    email: null,
+    telefono: null,
+    direccion: null,
+    celular: null,
+    selected: null,
 
-  
+  },
+  methods:{
+    revisar(e){
+      this.errores = [];
+
+      this.dato = this.selected
+      if(this.nombre == null || this.nombre.length == 0 || /^\s+$/.test(this.nombre)){
+        this.errores.push('Error!! en el campo nombre');   
+      }
+      if(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(this.email))){
+        this.errores.push('Tu correo electronico no es valido');
+      }
+      if(this.telefono == null || this.telefono == 0 && this.celular == null || this.celular == 0){
+        this.errores.push('Tienes que ingresar un numero telefonico y/o un numero Celular');
+      }else{
+        if( !(/^\d{7}$/.test(this.telefono)) && !(/^\d{10}$/.test(this.celular))){
+          this.errores.push('tu numero de telefono debe tener 7 digitos  o tu celular 10 digitos');    
+        }
+      
+      }
+      if(!this.errores.length){
+        return true;
+      }
+      e.preventDefault();
+      
+      
+    }
+  }
+})
